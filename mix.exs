60055f6b7a54d38d006732ad72a48f7aa55c5c08
Jonatan Männchen <jonatan@maennchen.ch>
2020-06-24 15:33:39 +0200

    Release v0.5.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule PhoenixClientSsl.Mixfile do
   def project do
     [
       app: :phoenix_client_ssl,
-      version: "0.4.1",
+      version: "0.5.0",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: description(),
